2e46b648e66e6f447634f06d7e3aee566f781434
Chandramouli Bhattacharya <bhattacharyac@gmail.com>
2013-07-11 21:37:51 +0000

    modified web.js to read from index.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buff = fs.readFileSync('./index.html');
+var text = buff.toString();
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
